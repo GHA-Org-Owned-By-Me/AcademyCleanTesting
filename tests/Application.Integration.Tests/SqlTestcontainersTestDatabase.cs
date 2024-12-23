a6548cf36640737d656e127c19f25011288cceff
Hoa VinhNguyen <Hoa.VinhNguyen@coxautoinc.com>
2024-12-22 20:54:48 -0600

    Rolled back to the sql server ubuntu image.

--- tests/Application.Integration.Tests/SqlTestcontainersTestDatabase.cs
+++ tests/Application.Integration.Tests/SqlTestcontainersTestDatabase.cs
@@ -19,6 +19,7 @@ public class SqlTestcontainersTestDatabase : ITestDatabase
     public SqlTestcontainersTestDatabase()
     {
         _container = new MsSqlBuilder()
+            .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
             .WithAutoRemove(true)
             .Build();
     }
